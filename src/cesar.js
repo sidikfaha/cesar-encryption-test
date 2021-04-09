c91afc9e010ad7d01af24343e07136b3815454ac
Joe Codeur <joefaha1@gmail.com>
2021-04-09 13:30:36 +0100

    wip

---
+++ src/cesar.js
@@ -0,0 +1,8 @@
+
+module.exports = (texte, cle) => {
+    let valeur = '' // Ne pas touvher cette ligne
+
+    // Ton code ici...
+
+    return valeur // Ne pas toucher cette ligne
+}
---
+++ src/cesar.test.js
@@ -0,0 +1,9 @@
+const sum = require("./cesar")
+
+test('Test de Séraphine', () => {
+    expect(sum("TEXTE", 3)).toBe("WHAWH")
+})
+
+test('Test de Sidik', () => {
+    expect(sum("TEXTE", 3)).toBe("WHAWH")
+})
